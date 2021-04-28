c8f108dee99c44282514ef57629096fd6e0c9eab
toxoliva <darwindarwindarwin2000@gmail.com>
2021-04-28 01:16:01 -0300

    Agrege los adicionales al store y cree la funcionalidad basica con ellos

--- combigo-api/src/api/additionals.js
+++ combigo-api/src/api/additionals.js
@@ -2,92 +2,84 @@ const express = require('express');
 
 const router = express.Router();
 
-const ID_BASE = 'CGOV';
+const vehicles = require('./store').additionals;
 
-const vehicles = require('./store').vehicles;
-
-// Get all vehicles
+// Get all additionals
 router.get('/', (req, res) => {
-  res.json(vehicles);
+  res.json(additionals);
 });
 
-// Search for vehicle with id
+// Search for aditional with id
 router.get('/:id', (req, res) => {
   const {id} = req.params;
-  const result = vehicles.find(veh => veh.id === id);
+  const result = additionals.find(additional => additional.id === id);
 
   if (!result) {
-    res.status(404).send(`Vehicle not found`);
+    res.status(404).send(`Aditional not found`);
   }
 
   res.json(result);
 });
 
-// Create vehicle
+// Create Aditional
 router.post('/', (req, res) => {
-  const {name, brand, plate, capacity} = req.body;
+  const {name} = req.body;
 
-  if (!req.body || !capacity) {
+  if (!req.body) {
     res.status(400).send(`Bad Request`)
   }
 
-  const exists = vehicles.find(veh => veh.plate === plate);
+  const exists = additionals.find(additional => additional.name === name);
 
   if (exists) {
-    res.status(409).send(`Vehicle already exists`);
+    res.status(409).send(`Aditional already exists`);
   }
 
-  const newVeh = [...vehicles, {
-    id: `${ID_BASE}${vehicles.length + 1}`,
+  const newAditionals = [...additionals, {
+    id: `${additionals.length + 1}`,
     name,
-    brand,
-    plate,
-    capacity,
   }];
 
-  vehicles = newVeh;
+  additionals = newAditionals;
 
-  res.send(vehicles);
+  res.send(additionals);
 });
 
-// Modify vehicle with id
+// Modify Aditional with id 
 router.put('/:id', (req, res) => {
   const {id} = req.params;
-  const {name, brand, plate, capacity} = req.body;
+  const {name} = req.body;
 
-  if (!req.body || !capacity) {
+  if (!req.body) {
     res.status(400).send(`Bad Request`)
   }
 
-  const exists = vehicles.findIndex(veh => veh.id === id);
+  const exists = additionals.findIndex(additional => additional.id === id);
 
   if (exists === -1) {
-    res.status(409).send(`Vehicle does not exists`);
+    res.status(409).send(`Aditional does not exists`);
   }
 
-  vehicles[exists] = {
+  additionals[exists] = {
     id,
     name,
-    brand,
-    plate,
-    capacity,
   };
 
-  res.send(vehicles);
+  res.send(additionals);
 });
 
 // Delete vehicle with id
 router.delete('/:id', (req, res) => {
   const {id} = req.params;
-  const index = vehicles.findIndex(veh => veh.id === id);
+  const index = additionals.findIndex(additional => additional.id === id);
 
   if (index === -1) {
-    res.status(404).send(`Vehicle not found`);
+    res.status(404).send(`Aditional not found`);
   }
 
-  vehicles.splice(index, 1);
+  additionals.splice(index, 1);
 
-  res.json(vehicles);
+  res.json(additionals);
 });
 
 module.exports = router;
