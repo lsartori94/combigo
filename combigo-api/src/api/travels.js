b4385f95df0c6d6198758952614126955c50ab7b
Toxoliva <darwindarwindarwin2000@gmail.com>
2021-05-05 21:35:47 -0300

    Hice unos pequeños cambios a los de travel que agrege ayer

--- combigo-api/src/api/travels.js
+++ combigo-api/src/api/travels.js
@@ -69,6 +69,66 @@ travelr.put('/:id', (req, res) => {
   res.send(travels);
 });
 
+// Add passenger
+travelr.put('/:id', (req, res) => {
+  const {id} = req.params;
+  const {passenger} = req.body;
+
+  if (!req.body) {
+    return res.status(400).send(`Bad Request`)
+  }
+
+  const exists = travels.findIndex(travel => travel.id === id);
+
+  if (exists === -1) {
+    return res.status(409).send(`travel does not exists`);
+  }
+
+  travels[exists].passengers.push( passenger )
+
+  res.send(passenger);
+});
+
+// Add possibleAdditional
+travelr.put('/:id', (req, res) => {
+  const {id} = req.params;
+  const {additional} = req.body;
+
+  if (!req.body) {
+    return res.status(400).send(`Bad Request`)
+  }
+
+  const exists = travels.findIndex(travel => travel.id === id);
+
+  if (exists === -1) {
+    return res.status(409).send(`travel does not exists`);
+  }
+
+  travels[exists].posibleAdditionals.push( additional )
+
+  res.send(additional);
+});
+
+// Add boughtAdditional // Esto se podria hacer muy distinto para que lleve cuenta de la cantidad de cada adicional...
+travelr.put('/:id', (req, res) => {
+  const {id} = req.params;
+  const {additional} = req.body;
+
+  if (!req.body) {
+    return res.status(400).send(`Bad Request`)
+  }
+
+  const exists = travels.findIndex(travel => travel.id === id);
+
+  if (exists === -1) {
+    return res.status(409).send(`travel does not exists`);
+  }
+
+  travels[exists].boughtAdditionals.push( additional )
+
+  res.send(additional);
+});
+
 // Delete travel with id
 travelr.delete('/:id', (req, res) => {
   const {id} = req.params;
