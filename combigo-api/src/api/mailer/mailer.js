d58b14a807741aae7b29e55b592bfc969b7db291
Luca Sartori <lucasartori94@gmail.com>
2021-06-17 21:24:54 -0300

    Adding mailer

---
+++ combigo-api/src/api/mailer/mailer.js
@@ -0,0 +1,110 @@
+const nodemailer = require("nodemailer");
+
+const activationTemplate = require('./activationTemplate');
+const cancelationTemplate = require('./cancelationTemplate');
+const vipStartTemplate = require('./vipStartedTemplate');
+const vipEndedTemplate = require('./vipEndedTemplate');
+const bookingConfirmedTemplate = require('./bookingConfirmedTemplate');
+const bookingCanceledTemplate = require('./bookingCanceledTemplate');
+
+let initialized = false;
+let transporter;
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+
+  if (initialized) return;
+
+  let testAccount = await nodemailer.createTestAccount();
+  initialized = true;
+  // create reusable transporter object using the default SMTP transport
+  transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+}
+
+async function sendEmail(to, subject, text, html) {
+  const from = '"Equipo de Combi19" <no-reply@combi19.com>';
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from,
+    to,
+    subject,
+    text,
+    html
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+function init() {
+  main().catch(console.error);
+}
+
+function sendActivationEmail(destination) {
+  const subject = "Activacion de su cuenta en Combi19";
+  const text = "Su cuenta en Combi19 ha sido creada, por favor navegue al sitio y utilice el su email y la contraseña provista por el chofer.";
+  const html = activationTemplate.getHtml();
+  sendEmail(destination, subject, text, html);
+}
+
+function sendTripCancelationEmail(destination, tripOrigin, tripDestination, tripDate, tripHour) {
+  const subject = "Su Viaje ha Sido Cancelado";
+  const text = "Su cuenta en Combi19 ha sido creada, por favor navegue al sitio y utilice el su email y la contraseña provista por el chofer.";
+  cancelationTemplate.setVariables(tripOrigin, tripDestination, tripDate, tripHour);
+  const html = cancelationTemplate.getHtml();
+  sendEmail(destination, subject, text, html);
+}
+
+function sendBookingCancelationEmail(destination, tripOrigin, tripDestination, tripDate, tripHour, ammount) {
+  const subject = "Reserva Cancelada";
+  const text = "Usted ha cancelado su reserva";
+  bookingCanceledTemplate.setVariables(tripOrigin, tripDestination, tripDate, tripHour, ammount)
+  const html = bookingCanceledTemplate.getHtml();
+  sendEmail(destination, subject, text, html);
+}
+
+function sendBookingConfirmationEmail(destination, tripOrigin, tripDestination, tripDate, tripHour) {
+  const subject = "Su Reserva ha Sido Confirmada";
+  const text = "Su reserva ha sido confirmada";
+  bookingConfirmedTemplate.setVariables(tripOrigin, tripDestination, tripDate, tripHour)
+  const html = bookingConfirmedTemplate.getHtml();
+  sendEmail(destination, subject, text, html);
+}
+
+function vipSubscriptionInitiatedEmail(destination) {
+  const subject = "Bienvenido a Combi19 VIP";
+  const text = "Usted ahora es VIP";
+  const html = vipStartTemplate.getHtml();
+  sendEmail(destination, subject, text, html);
+}
+
+function vipSubscriptionEndedEmail(destination) {
+  const subject = "Cancelacion de VIP";
+  const text = "Su subscripcion VIP ha sido cancelada";
+  const html = vipEndedTemplate.getHtml();
+  sendEmail(destination, subject, text, html);
+}
+
+module.exports = {
+  init,
+  sendActivationEmail,
+  sendTripCancelationEmail,
+  sendBookingConfirmationEmail,
+  sendBookingCancelationEmail,
+  vipSubscriptionInitiatedEmail,
+  vipSubscriptionEndedEmail
+};
--- combigo-api/src/app.js
+++ combigo-api/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mailer = require('./api/mailer/mailer');
 
 require('dotenv').config();
 
@@ -15,11 +16,80 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+mailer.init();
+
 app.get('/', (req, res) => {
   res.json({
     message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
   });
 });
+
+// Rutas demo mailer
+
+app.get('/demo/mailer/activation', (req, res) => {
+  mailer.sendActivationEmail('test@test.com');
+  res.json({
+    message: 'MAILER DEMO WOOOO ====> Activacion de cuenta'
+  });
+})
+
+app.get('/demo/mailer/tripCancelation', (req, res) => {
+  mailer.sendTripCancelationEmail(
+    'test@test.com',
+    "La Plata", //viaje origen
+    "Buenos Aires", //viaje destino
+    "2021-06-09", //viaje fecha
+    "09:00" //viaje hora
+  );
+  res.json({
+    message: 'MAILER DEMO WOOOO ====> Viaje cancelado por admin ====> Mira la consola para tener la URL del mensaje'
+  });
+})
+
+app.get('/demo/mailer/vipStart', (req, res) => {
+  mailer.vipSubscriptionInitiatedEmail(
+    'test@test.com'
+  );
+  res.json({
+    message: 'MAILER DEMO WOOOO ====> Subscripcion VIP ====> Mira la consola para tener la URL del mensaje'
+  });
+})
+
+app.get('/demo/mailer/vipEnd', (req, res) => {
+  mailer.vipSubscriptionEndedEmail(
+    'test@test.com'
+  );
+  res.json({
+    message: 'MAILER DEMO WOOOO ====> Desubscripcion VIP ====> Mira la consola para tener la URL del mensaje'
+  });
+})
+
+app.get('/demo/mailer/bookingConfirmed', (req, res) => {
+  mailer.sendBookingConfirmationEmail(
+    'test@test.com',
+    "La Plata", //viaje origen
+    "Buenos Aires", //viaje destino
+    "2021-06-09", //viaje fecha
+    "09:00" //viaje hora
+  );
+  res.json({
+    message: 'MAILER DEMO WOOOO ====> Reserva Confirmada ====> Mira la consola para tener la URL del mensaje'
+  });
+})
+
+app.get('/demo/mailer/bookingCanceled', (req, res) => {
+  mailer.sendBookingCancelationEmail(
+    'test@test.com',
+    "La Plata", //viaje origen
+    "Buenos Aires", //viaje destino
+    "2021-06-09", //viaje fecha
+    "09:00", //viaje hora,
+    "50%" //porcentaje de reintegro
+  );
+  res.json({
+    message: 'MAILER DEMO WOOOO ====> Desubscripcion VIP ====> Mira la consola para tener la URL del mensaje'
+  });
+})
 
 app.use('/api/v1', api);
 
