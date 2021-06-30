bf0f171df26701544dc2cc5d4f5e1597ee10d6ac
fgerst <gerstmayerf@gmail.com>
2021-06-30 01:50:49 -0300

    store consistente

--- combigo-api/src/api/store.js
+++ combigo-api/src/api/store.js
@@ -1,7 +1,7 @@
 const { ROLES, TRAVEL_STATES, BOOKING_STATES, LEGAL_STATUS, VIP_STATUS } = require('./constants');
 
 let users = [
-  { //admin
+  { //ADMIN
     id: 'CGOU0',
     username: 'admin',
     email: 'admin',
@@ -11,44 +11,56 @@ let users = [
     role: ROLES.ADMIN,
     active: true
   },
-  { //drivers
+  { //DRIVERS
     id: 'CGOU1',
     username: 'jose.traffic',
     email: 'joset@gmail.com',
     name: 'Jose',
     password: '123@Pass',
-    bdate: '1960-12-12',
-    dni: '34506758',
+    bdate: '1962-12-12',
+    dni: '34406758',
     completedTravels: [],
     role: ROLES.DRIVER,
     active: true
   },
   {
-    id: 'CGOU5',
-    username: 'manuel.volvo',
-    email: 'manuelv@gmail.com',
+    id: 'CGOU2',
+    username: 'manuel.benz',
+    email: 'manuelb@gmail.com',
     name: 'Manuel',
     password: '123@Pass',
-    bdate: '1980-12-12',
-    dni: '34516758',
+    bdate: '1981-10-20',
+    dni: '38516758',
     completedTravels: [],
     role: ROLES.DRIVER,
     active: true
   },
   {
-    id: 'CGOU6',
-    username: 'john.volvo',
-    email: 'johnv@gmail.com',
-    name: 'John',
+    id: 'CGOU3',
+    username: 'jorge.volvo',
+    email: 'jorgev@gmail.com',
+    name: 'Jorge',
     password: '123@Pass',
-    bdate: '1970-12-12',
-    dni: '34616758',
+    bdate: '1974-6-10',
+    dni: '36194680',
     completedTravels: [],
     role: ROLES.DRIVER,
     active: true
   },
-  { //clients
-    id: 'CGOU2',
+  {
+    id: 'CGOU4',
+    username: 'raul.volvo',
+    email: 'raulv@gmail.com',
+    name: 'Raul',
+    password: '123@Pass',
+    bdate: '1972-11-12',
+    dni: '35678778',
+    completedTravels: [],
+    role: ROLES.DRIVER,
+    active: true
+  },
+  { //CLIENTS
+    id: 'CGOU5',
     username: 'juan.perez',
     email: 'juanp@gmail.com',
     name: 'Juan',
@@ -69,7 +81,7 @@ let users = [
       issuer: 'Visa',
       number: '4983794057102007',
       cardHolder: 'Juan Perez',
-      expDate: '2021-11-12',
+      expDate: '2021-10-10',
       cvv:'123'
     },
     vip: {
@@ -81,7 +93,7 @@ let users = [
     registerDate: '2021-10-01T12:24:00'
   },
   {
-    id: 'CGOU3',
+    id: 'CGOU6',
     username: 'diego.sosa',
     email: 'diegos@gmail.com',
     name: 'Diego',
@@ -92,12 +104,12 @@ let users = [
       {
         travelId: 'CGOT1',
         bookingId: 'CGOB0',
-        legalStatus: LEGAL_STATUS.PENDING,
+        legalStatus: LEGAL_STATUS.APPROVED,
         boughtAdditionals: [ 
           'CGOA1',
           'CGOA3',
         ],
-        status: BOOKING_STATES.PENDING,
+        status: BOOKING_STATES.COMPLETED,
         payment: 350
       },
       {
@@ -119,9 +131,9 @@ let users = [
       {
         travelId: 'CGOT4',
         bookingId: 'CGOB3',
-        legalStatus: LEGAL_STATUS.APPROVED,
+        legalStatus: LEGAL_STATUS.PENDING,
         boughtAdditionals: [],
-        status: BOOKING_STATES.ACTIVE,
+        status: BOOKING_STATES.PENDING,
         payment: 120
       }
     ],
@@ -135,7 +147,7 @@ let users = [
     registerDate: '2021-01-20T14:30:00'
   },
   {
-    id: 'CGOU4',
+    id: 'CGOU7',
     username: 'carlos.lopez',
     email: 'carlosl@gmail.com',
     name: 'Carlos',
@@ -245,27 +257,27 @@ let routes = [
 let travels = [
   {
     id: 'CGOT1',
-    dateAndTime: '2021-06-24T22:40',
+    dateAndTime: '2021-05-10T22:40',
     route: 'CGOR1',
     stock: 1,
     passengers: [
       {
-        id: 'CGOU3',
+        id: 'CGOU6',
         ticketId: 'CGOTKT1',
-        legalStatus: LEGAL_STATUS.PENDING,
-        bookingStatus: BOOKING_STATES.PENDING,
+        legalStatus: LEGAL_STATUS.APPROVED,
+        bookingStatus: BOOKING_STATES.COMPLETED,
         creditCard: '4444444444444444',
         payment: 350,
         boughtAdditionals: [
           'CGOA1',
           'CGOA3',
         ],
-        accepted: false,
+        accepted: true,
       }
     ],
     driver: 'CGOU1',
     vehicle: 'CGOV4',
-    status: TRAVEL_STATES.NOT_STARTED,
+    status: TRAVEL_STATES.FINISHED,
     availableAdditionals: [
       'CGOA1',
       'CGOA3'
@@ -275,19 +287,19 @@ let travels = [
   },
   {
     id: 'CGOT2',
-    dateAndTime: '2021-07-01T08:30',
+    dateAndTime: '2021-06-01T09:00',
     route: 'CGOR3',
     stock: 9,
     passengers: [
       {
-        id: 'CGOU3',
+        id: 'CGOU6',
         ticketId: 'CGOTKT1',
         legalStatus: LEGAL_STATUS.APPROVED,
         bookingStatus: BOOKING_STATES.COMPLETED,
         creditCard: '4444444444444444',
         payment: 160,
         boughtAdditionals: [],
-        accepted: false,
+        accepted: true,
       }
     ],
     driver: 'CGOU1',
@@ -303,12 +315,12 @@ let travels = [
   },
   {
     id: 'CGOT3',
-    dateAndTime: '2021-08-01T08:30',
+    dateAndTime: '2021-06-05T08:30',
     route: 'CGOR3',
     stock: 11,
     passengers: [
       {
-        id: 'CGOU3',
+        id: 'CGOU6',
         ticketId: 'CGOTKT1',
         legalStatus: LEGAL_STATUS.APPROVED,
         bookingStatus: BOOKING_STATES.COMPLETED,
@@ -318,7 +330,7 @@ let travels = [
         accepted: true,
       },
       {
-        id: 'CGOU2',
+        id: 'CGOU5',
         ticketId: 'CGOTKT2',
         legalStatus: LEGAL_STATUS.REJECTED,
         bookingStatus: BOOKING_STATES.CANCELED,
@@ -341,24 +353,24 @@ let travels = [
   },
   {
     id: 'CGOT4',
-    dateAndTime: '2021-06-09T09:00',
+    dateAndTime: '2021-07-01T09:20',
     route: 'CGOR4',
     stock: 11,
     passengers: [
       {
-        id: 'CGOU3',
+        id: 'CGOU6',
         ticketId: 'CGOTKT1',
-        legalStatus: LEGAL_STATUS.APPROVED,
-        bookingStatus: BOOKING_STATES.ACTIVE,
+        legalStatus: LEGAL_STATUS.PENDING,
+        bookingStatus: BOOKING_STATES.PENDING,
         creditCard: '4444444444444444',
         payment: 120,
         boughtAdditionals: [],
-        accepted: true,
+        accepted: false,
       }
     ],
     driver: 'CGOU1',
     vehicle: 'CGOV1',
-    status: TRAVEL_STATES.IN_PROGRESS,
+    status: TRAVEL_STATES.NOT_STARTED,
     availableAdditionals: [
       'CGOA2',
       'CGOA4',
@@ -371,7 +383,7 @@ let travels = [
     id: 'CGOT5',
     dateAndTime: '2021-08-24T22:40',
     route: 'CGOR1',
-    stock: 1,
+    stock: 2,
     passengers: [],
     driver: 'CGOU1',
     vehicle: 'CGOV4',
